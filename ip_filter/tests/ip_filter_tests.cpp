06dee5c0361db1cf1bb5cb79b6f510d07f5ad764
Andreanapolicy <daa.and@mail.ru>
2024-09-11 21:21:07 +0300

    ip_filter. Started writing tests

--- ip_filter/tests/ip_filter_tests.cpp
+++ ip_filter/tests/ip_filter_tests.cpp
@@ -2,10 +2,56 @@
 #include "../src/ip_list.h"
 #include <sstream>
 
+class IPListTest : public testing::Test
+{
+protected:
+    IPListTest() = default;
+    
+    ~IPListTest() override = default;
+    
+    void SetUp() override
+    {    
+        std::ostringstream oss;
+        oss << "10.10.10.10\t2\t4" << std::endl;
+        oss << "2.2.100.30\t2\t4" << std::endl;
+        oss << "1.2.100.30\t2\t4" << std::endl;
+        oss << "46.70.100.30\t2\t4" << std::endl;
+        oss << "46.1.100.30\t2\t4" << std::endl;
+        
+        std::istringstream iss(oss.str());
+        iss >> ipList;
+    }
+    
+    void TearDown() override {}
+
+    ip_address::IPList ipList;
+};
+
+
 TEST(IPList, EmptyListAfterCreating)
 {
     ip_address::IPList ipList;
-    EXPECT_EQ(ipList.Empty(), true);
+    ASSERT_TRUE(ipList.Empty());
+}
+
+TEST(IPList, TestReadFromStream)
+{
+    ip_address::IPList ipList;
+    std::istringstream iss("10.10.10.10\t2\t4\n2.2.100.30\t2\t4");
+
+    iss >> ipList;
+
+    ASSERT_FALSE(ipList.Empty());
+    ASSERT_EQ(ipList.Count(), 2);
+}
+
+TEST_F(IPListTest, TestWriteToStream)
+{
+    std::ostringstream oss;
+    
+    oss << ipList;
+
+    ASSERT_EQ(oss.str(), "10.10.10.10\n2.2.100.30\n1.2.100.30\n46.70.100.30\n46.1.100.30\n");
 }
 
 int main(int argc, char **argv)
