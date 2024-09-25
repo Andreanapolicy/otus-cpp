c560dfdbeea0bc7b831b35d15e18c02be3b29954
Andreanapolicy <daa.and@mail.ru>
2024-09-25 19:46:15 +0300

    sfinae. Expand solution for vector to list also

--- sfinae/main.cpp
+++ sfinae/main.cpp
@@ -18,11 +18,17 @@ print_ip(const T& str)
 }
 
 template <typename T>
-typename std::enable_if<std::is_same_v<T, std::vector<typename T::value_type>>, void>::type
+typename std::enable_if<std::disjunction_v<
+    std::is_same<T, std::vector<typename T::value_type>>,
+    std::is_same<T, std::list<typename T::value_type>>>, void>::type
 print_ip(const T& container)
 {
+    if (container.empty())
+    {
+        return;
+    }
     std::string s = std::to_string(*(container.begin()));
-    for (auto it = container.cbegin() + 1; it != container.cend(); it++)
+    for (auto it = ++(container.cbegin()); it != container.cend(); it++)
     {
         s += "." + std::to_string(*(it));
     }
@@ -38,6 +44,6 @@ int main()
     print_ip( int64_t{8875824491850138409} );// 123.45.67.89.101.112.131.41
     print_ip( std::string{"Hello, World!"} ); // Hello, World!
     print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400
-    //print_ip( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100
+    print_ip( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100
     return 0;
 }
