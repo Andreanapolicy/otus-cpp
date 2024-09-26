1aba5c4cb03a7197bec61538ea8b79f47d84ad5d
Andreanapolicy <daa.and@mail.ru>
2024-09-26 21:19:39 +0300

    sfinae. Added doxygen config and comments for them

--- sfinae/main.cpp
+++ sfinae/main.cpp
@@ -3,6 +3,11 @@
 #include <list>
 #include <iostream>
 
+/// <summary>
+/// Prints string type value.
+/// </summary>
+/// <typeparam name="T">.The type of the input value Must be an string type</typeparam>
+/// <param name="value">The string value to be printed</param>
 template <typename T>
 typename std::enable_if<std::is_same_v<T, std::string>, void>::type
 print_ip(const T& str)
@@ -10,6 +15,19 @@ print_ip(const T& str)
     std::cout << str << std::endl;
 }
 
+/// <summary>
+/// Construct the IP address of integral type to string.
+/// </summary>
+/// <typeparam name="T">The type of the input value. Must be an integral type</typeparam>
+/// <param name="value">The integral value to be printed as an IP address.</param>
+///  
+/// Example usage:
+/// \code{.cpp}
+/// print_ip(int8_t{-1}); // 255
+/// print_ip(int16_t{0}); // 0.0
+/// print_ip(int32_t{2130706433}); // 127.0.0.1
+/// print_ip(int64_t{8875824491850138409});// 123.45.67.89.101.112.131.41
+/// \endcode
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value, void>::type
 print_ip(T number)
@@ -36,6 +54,11 @@ print_ip(T number)
     print_ip(result);
 }
 
+/// <summary>
+/// Construct the IP address representation of elements in a container to string.
+/// </summary>
+/// <typeparam name="T">The type of the container. Must be either std::vector or std::list.</typeparam>
+/// <param name="container">The container whose elements are to be printed as an IP address.</param>
 template <typename T>
 typename std::enable_if<std::disjunction_v<
     std::is_same<T, std::vector<typename T::value_type>>,
